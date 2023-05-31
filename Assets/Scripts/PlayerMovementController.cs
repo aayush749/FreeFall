932c4eac609ff95d04a2dd9f559f5942f00134c0
Aayush Anand <aaayushanand5@gmail.com>
2023-05-31 17:48:23 +0530

    Added cinemachine, and player motion controller script

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    // this field is set only once, when the velocity of player becomes zero at the start of the scene
+    bool hasStopped = false;
+    float limitToDetectStop = 1e-1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Give an initial velocity to the player
+        transform.GetComponent<Rigidbody>().velocity = 1.5f * Vector3.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfStopped();
+
+        if (hasStopped)
+        {
+            // TODO: send a message to the UI manager to show 'START' text
+        }
+    }
+
+    private void CheckIfStopped()
+    {
+        // check if the velocity of the player has gone within a specific limit
+        // do this only if the bool field 'hasStopped' hasn't been set yet
+        if (!hasStopped)
+        {
+            if (transform.GetComponent<Rigidbody>().velocity.magnitude < limitToDetectStop)
+            {
+                hasStopped = true;
+                Debug.LogWarning("Stopped!");
+            }
+        }
+    }
+}
