d132dcf8e7ee51df5660360f325971406b445923
Aayush Anand <aaayushanand5@gmail.com>
2023-06-01 14:41:00 +0530

    Implemented infinite scroller, but has bugs

--- Assets/Scripts/InfiniteScroller.cs
+++ Assets/Scripts/InfiniteScroller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class InfiniteScroller : MonoBehaviour
@@ -12,12 +13,16 @@ public class InfiniteScroller : MonoBehaviour
     [SerializeField]
     PlayerMovementController playerMovementController;
 
+    GameObject oldPlane;
+
     // Start is called before the first frame update
     void Start()
     {
         // Subscribe to OnFallOffDetect Event for the player
         playerMovementController.OnFallOffDetect += ShufflePlanes;
+        playerMovementController.OnEnterNewPlane += DestroyOldPlane;
     }
+
 
     // Update is called once per frame
     void Update()
@@ -25,13 +30,62 @@ public class InfiniteScroller : MonoBehaviour
         
     }
 
-    void ShufflePlanes(object sender, EventArgs args)
+    private void ShufflePlanes(object sender, EventArgs args)
     {
         // collided with the fall off detection cube
         Debug.LogWarning("Collided with the fall-off plane");
 
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPaused = true;
-#endif
+
+        GameObject currentPlane = playerMovementController.GetCurrentPlane();
+
+        var length = currentPlane.GetComponent<MeshRenderer>().bounds.size.z;
+        var transform = currentPlane.transform;
+
+        var angle = 90 - currentPlane.transform.rotation.eulerAngles.x;
+
+        Vector3 newPos = new Vector3(
+            transform.position.x, transform.position.y - length * (float) Math.Sin(ToRadians(angle)) / 1.5f,
+            transform.position.z + 2 * length * (float) Math.Cos(ToRadians(angle))
+            );
+
+        // Choose a random plane from the list of current planes
+        int idx = GetRandomPlaneIdxToInstantiate();
+
+        GameObject newPlane = Instantiate<GameObject>(planes[idx], newPos, currentPlane.transform.rotation) as GameObject;
+
+        // Set the current plane in playerMovementController
+        playerMovementController.SetCurrentPlane(newPlane);
+
+        // Set the old plane in this script to be the currentPlane,
+        // which would be destroyed when player enters currently created new plane, and the OnEnterNewPlane method is triggered
+        oldPlane = currentPlane;
+
+//#if UNITY_EDITOR
+//        UnityEditor.EditorApplication.isPaused = true;
+//#endif
+    }
+
+    private void DestroyOldPlane(object sender, EventArgs e)
+    {
+//#if UNITY_EDITOR
+//        UnityEditor.EditorApplication.isPaused = true;
+//#endif
+        Debug.LogWarning("Entering new plane");
+        if (oldPlane != null)
+        {
+            Destroy(oldPlane);
+            Debug.LogWarning("Destroyed old plane");
+        }
+    }
+
+    private int GetRandomPlaneIdxToInstantiate()
+    {
+        return (int) Mathf.Round(
+            UnityEngine.Random.Range(0.0f, planes.Count - 1));
+    }
+
+    public static double ToRadians(double angleInDegrees)
+    {
+        return (Math.PI / 180) * angleInDegrees;
     }
 }
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -12,6 +12,9 @@ public class PlayerMovementController : MonoBehaviour
     bool hasStopped = false;
     float limitToDetectStop = 1e-1f;
 
+    [SerializeField, Range(0.0f, 2.0f)]
+    private float initialSpeed = 1.0f;
+
     [SerializeField]
     bool canBeginMotion = false;
 
@@ -37,17 +40,27 @@ public class PlayerMovementController : MonoBehaviour
     // This field is to track if the speed limit has been hit at least once during the gameplay
     bool hasHitSpeedLimitOnce = false;
 
-    [SerializeField, Tooltip("The name of the plane use to detect fall from the plane, before it happens")]
+    // Plane on which the player is currently on
+    private GameObject plane;
+
+    [SerializeField, Tooltip("The name of the plane used to detect fall from the plane, before it happens")]
     private string fallDetectionPlaneName;
 
+    [SerializeField, Tooltip("The name of the plane used to detect entry in a new plane")]
+    private string entryDetectionPlaneName;
+
     public event EventHandler OnFallOffDetect;
+    public event EventHandler OnEnterNewPlane;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         // Give an initial velocity to the player
-        rb.velocity = 1.5f * Vector3.forward;
+        rb.velocity = initialSpeed * Vector3.forward;
+
+        // Find the only plane instantiated in the scene, and store it
+        plane = GameObject.Find("Plane");
     }
 
     // Update is called once per frame
@@ -55,22 +68,35 @@ public class PlayerMovementController : MonoBehaviour
     {
         CheckIfStopped();
         HandlePlayerMotion();
-        UpdateCurrentPlaneIndex();
-    }
-
-    private void UpdateCurrentPlaneIndex()
-    {
-        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.LogWarning("Collided");
-        if (other.isTrigger && other.name == fallDetectionPlaneName)
+        if (other.isTrigger)
         {
-            // invoke the event of fall detection
-            OnFallOffDetect.Invoke(this, EventArgs.Empty);
+            if (other.name == fallDetectionPlaneName)
+            {
+                // invoke the event of fall detection
+                OnFallOffDetect.Invoke(this, EventArgs.Empty);
+            }
+            else if (other.name == entryDetectionPlaneName)
+            {
+                // this is a new plane the player has entered
+                // invoke the event of entering new plane
+                OnEnterNewPlane.Invoke(this, EventArgs.Empty);
+            }
         }
+    }
+
+    public GameObject GetCurrentPlane()
+    {
+        return plane;
+    }
+
+    public void SetCurrentPlane(GameObject plane)
+    {
+        this.plane = plane;
     }
 
     private void HandlePlayerMotion()
@@ -86,6 +112,10 @@ public class PlayerMovementController : MonoBehaviour
 
         if (canBeginMotion)
         {
+            // if player speed is over the max limit, fix it to max limit
+
+            PreventSpeeding();
+
             if (Input.GetKey(KeyCode.W))
             {
                 // move forward
@@ -118,6 +148,22 @@ public class PlayerMovementController : MonoBehaviour
                 rb.angularVelocity = Vector3.zero;
                 rb.velocity += Vector3.right * forwardForceMagnitude * dashSpeedMultiplier;
             }
+        }
+    }
+
+    private void PreventSpeeding()
+    {
+        Debug.Log("Speed Throttled");
+        if (rb.velocity.magnitude > maxSpeedLimit)
+        {
+            // calculate the new z component for velocity
+            // such that speed is equal to max limit
+            var newVelocityZComponentSquared = Math.Pow(maxSpeedLimit, 2) - Mathf.Pow(rb.velocity.x, 2) - Mathf.Pow(rb.velocity.y, 2);
+
+            // update the velocity z component
+            rb.velocity = new Vector3(
+                rb.velocity.x, rb.velocity.y,
+                Mathf.Sqrt((float)newVelocityZComponentSquared));
         }
     }
 
